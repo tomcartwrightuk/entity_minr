45dfab81cbad4e6dffa97986cbe00cf09948ef2e
Tom Cartwright <tecartwright@gmail.com>
2013-11-26 08:34:26 +0000

    Refactor extract method

--- src/main/scala/entityextractor/services/ner.scala
+++ src/main/scala/entityextractor/services/ner.scala
@@ -10,42 +10,46 @@ object EntityWrangler {
   def extract(contents: String): List[NamedEntity] = {
    
     val classified = classifier.classify(contents)
-   
     var entities = List[NamedEntity]()
-    for (i <- 0 to classified.size() - 1) {
-      val sentence = classified.get(i)
-   
-      var prevWordClass = ""
-      var entity = NamedEntity("", "")
-   
-      for (j <- 0 to sentence.size() - 1) {
-        val word = sentence.get(j)
-        val wordClass = word.get(classOf[CoreAnnotations.AnswerAnnotation]).toString()
-   
-        if (!prevWordClass.equals(wordClass)) {
-          if (!prevWordClass.equals("O") && !prevWordClass.equals("")) {
-            entities = entities :+ entity
-            entity = NamedEntity("", "")
-          }
-        }
 
-        if (!wordClass.equals("O") && !wordClass.equals("")) {
-          if (!prevWordClass.equals(wordClass)) {
-            entity = entity.copy(entity_type = wordClass, entity_text = word.toString())
-          }
-          if (wordClass.equals(prevWordClass)) {
-            entity = entity.copy(entity_type = wordClass, entity_text = entity.entity_text + " " + word)
-          }
-        }
-        prevWordClass = wordClass
-   
-      }
+    for (i <- 0 to classified.size() - 1) {
+      var sentenceEntities = extractFromSentence(classified.get(i))
+      entities = entities ++ sentenceEntities
     }
     return entities
   }
 
+  def extractFromSentence(sentence: java.util.List[edu.stanford.nlp.ling.CoreLabel]): List[NamedEntity] = {
+
+    var sentenceEntities = List[NamedEntity]()
+    var prevWordClass = ""
+    var entity = NamedEntity("", "")
+ 
+    for (j <- 0 to sentence.size() - 1) {
+      val word = sentence.get(j)
+      val wordClass = word.get(classOf[CoreAnnotations.AnswerAnnotation]).toString()
+ 
+      if (!prevWordClass.equals(wordClass)) {
+        if (!prevWordClass.equals("O") && !prevWordClass.equals("")) {
+          sentenceEntities = sentenceEntities :+ entity
+          entity = NamedEntity("", "")
+        }
+      }
+
+      if (!wordClass.equals("O") && !wordClass.equals("")) {
+        if (!prevWordClass.equals(wordClass)) {
+          entity = entity.copy(entity_type = wordClass, entity_text = word.toString())
+        }
+        if (wordClass.equals(prevWordClass)) {
+          entity = entity.copy(entity_type = wordClass, entity_text = entity.entity_text + " " + word)
+        }
+      }
+      prevWordClass = wordClass
+ 
+    }
+    return sentenceEntities
+  }
+
 }
 
-case class NamedEntity(entity_type: String, entity_text: String) {
-  def this() = this("", "")
-}
+case class NamedEntity(entity_type: String, entity_text: String)
